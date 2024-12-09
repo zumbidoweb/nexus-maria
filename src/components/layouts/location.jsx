b44c2c15e08be8b58382b4ded8e21d1c6130923f
Fabio <zumbidoweb@gmail.com>
2024-12-10 00:33:32 +0200

    updates

--- src/components/layouts/location.jsx
+++ src/components/layouts/location.jsx
@@ -22,7 +22,7 @@ export default function LocationLayout({ children, posts, data, slug }) {
       <div className="lg:flex">
      
         <div className="lg:w-1/3 relative  fixed -top-10 left-0 z-10 ">
-          <div className=" pl-8 max-w-2xl lg:pr-0 pr-8 pt-32 w-full relative top-0 left-0 ">   
+          <div className="pl-2 lg:pl-6 max-w-2xl lg:pr-0 pr-2 pt-32 w-full relative top-0 left-0 ">   
             <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: .8 } }}>       
               <Map links={posts} /> 
               <div className="-mt-7 text-xs text-gray-500 text-center">click at the dots to navigate</div> 
@@ -30,7 +30,7 @@ export default function LocationLayout({ children, posts, data, slug }) {
           </div>   
         </div> 
 
-        <div className="lg:w-2/3 relative  pl-12 pr-16 bg-white dark:bg-gray-950 z-20" key={data.color}>
+        <div className="lg:w-2/3 relative  pl-6 xl:pl-12 pr-16 bg-white dark:bg-gray-950 z-20" key={data.color}>
 
           <LocomotiveScrollProvider options={{ smooth: true, tablet: { breakpoint: 0 }, tablet: { smooth: false } }} containerRef={containerRef} watch={[router.asPath]}>
             <section className="py-12 sm:py-16 md:py-20 lg:py-36  xl:py-32 2xl:py-40 " data-scroll-container ref={containerRef}>  
--- src/pages/index.js
+++ src/pages/index.js
@@ -19,10 +19,15 @@ import { TbHexagonLetterNFilled } from "react-icons/tb";
 export default function Index({ posts }) {
   return (
     <div className="relative h-screen w-screen overflow-hidden bg-white">
-      <div className="absolute px-16 right-0 left-0 z-20 top-[32%] ">
-        <div className="max-w-2xl mx-auto text-center bg-white 0 shadow-white dark:shadow-none p-10  text-gray-900 rounded ">
+
+      <div className="absolute  h-screen w-screen  z-10 opacity-30 md:opacity-100">  
+        <MapAnimated links={posts} />   
+      </div>  
+
+      <div className="relative  h-screen w-screen  flex items-center justify-center z-20 xl:px-16">
+        <div className="max-w-2xl mx-auto text-center md:bg-white 0 shadow-white dark:shadow-none p-10  text-gray-900 rounded ">
           <motion.h1 
-            className="text-7xl font-semibold mb-4 font-serif" 
+            className="text-7xl font-semibold mb-2 md:mb-4 font-serif" 
             initial={{ y: -10, opacity: 0 }}
             animate={{ y: 0, opacity: 1, transition: { duration: 1 } }}>   
             Nexus
@@ -32,12 +37,12 @@ export default function Index({ posts }) {
           </motion.p>
           <motion.div  
             initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: .5, delay: 3 } }} 
-            className="prose-sm  dark:prose-invert max-w-none mt-6">                 
+            className="prose-sm  dark:prose-invert max-w-none mt-4 md:mt-6">                 
             <p className="font-medium">This is an art project without clear dividing lines between artwork, research, social engagement and documentation. Like a plexus, it is an interwoven combination of elements in one structure.All elements are correlated and mingled in a dynamic mapping process. </p>
           </motion.div> 
           <motion.div  
             initial={{ opacity: 0, y: 10, }} animate={{ y: 0, opacity: 1, transition: { duration: .5, delay: 6.5 } }} 
-            className="mt-8">   
+            className="mt-4 md:mt-8 mb-24 md:mb-0">   
             <Link className="text font-serif uppercase font-semibold  tracking-wide  bg-black rounded-sm px-6 py-2 cursor-pointer hover:bg-red-700 inline-block text-white" href="/locations/about">  
               <div className="flex gap-3 items-center">       
                 View project
@@ -47,9 +52,7 @@ export default function Index({ posts }) {
         </div>
       </div>
 
-      <div className="relative  h-screen w-screen pt-10  pb-24 z-10 ">  
-        <MapAnimated links={posts} />   
-      </div>  
+   
       <Footer />
     </div >     
 
